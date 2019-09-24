f77dc9e523b1a2d15c2b5c6d812e0851c9349152
YOUR NAME <YOUR@EMAIL.com>
2019-09-24 23:12:46 +0900

    Webページ表示（ リンクタップ→サイト内リンクでもサイト外リンクでもアプリ内でWebページ開封 JS❌、 戻るボタン❌）

--- app/src/main/java/com/oshimamasara/webviewmaster000/MainActivity.java
+++ app/src/main/java/com/oshimamasara/webviewmaster000/MainActivity.java
@@ -26,14 +26,12 @@ public class MainActivity extends AppCompatActivity {
     private class MyWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if ("blog.codecamp.jp".equals(Uri.parse(url).getHost())) {
-                // This is my website, so do not override; let my WebView load the page
+            //if ("blog.codecamp.jp".equals(Uri.parse(url).getHost())) {
                 return false;
-            }
-            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            startActivity(intent);
-            return true;
         }
+            //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            //startActivity(intent);
+            //return true;
+        //}
     }
 }
