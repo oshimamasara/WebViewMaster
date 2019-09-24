2771511936aaed2aaabff17a0758dc7bf9148ab8
YOUR NAME <YOUR@EMAIL.com>
2019-09-24 23:07:50 +0900

    Webページ表示（ リンクタップ→サイト内リンクであればアプリ内でWebページ開封 リンクタップ→サイト外リンクであればブラウザでWebページ開封 JS❌、 戻るボタン❌）

--- app/src/main/java/com/oshimamasara/webviewmaster000/MainActivity.java
+++ app/src/main/java/com/oshimamasara/webviewmaster000/MainActivity.java
@@ -2,8 +2,11 @@ package com.oshimamasara.webviewmaster000;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
         myWebView.loadUrl("https://blog.codecamp.jp");
+
+        myWebView.setWebViewClient(new MyWebViewClient());
+    }
+
+    //アプリ内でブラウザオープン　特定ドメイン以外はブラウザ起動
+    // text https://developer.android.com/guide/webapps/webview#HandlingNavigation
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if ("blog.codecamp.jp".equals(Uri.parse(url).getHost())) {
+                // This is my website, so do not override; let my WebView load the page
+                return false;
+            }
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
     }
 }
